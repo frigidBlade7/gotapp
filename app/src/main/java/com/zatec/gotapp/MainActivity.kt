7db177c38bc6082417d1555d57018ea6cb1a81c2
Nate <kobbatt@gmail.com>
2022-11-28 19:56:07 +0000

    [update] added splash screen and dark/light theming

---
+++ app/src/main/java/com/zatec/gotapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.zatec.gotapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
